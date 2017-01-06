b3fad158b62964fd84ccd94bc6d44115a9fdeb1f
ronaldap2017 <ronalmeidarj@gmail.com>
2017-01-06 19:47:35 -0200

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,14 @@
+import visao.TelaPrincipal;
+import controle.ControlePrincipal;
+
+public class Principal {
+	private static TelaPrincipal tp;
+	public static void main(String[] args) {
+		
+						@SuppressWarnings("unused")
+						ControlePrincipal cf;
+						TelaPrincipal tc = new TelaPrincipal();
+						cf = new ControlePrincipal(tc);
+					
+	}
+}
